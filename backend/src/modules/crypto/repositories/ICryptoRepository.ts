08774ed1476c75021c827a8890e53bd5c6e2efc7
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-04-25 12:05:40 -0300

    feat: Add crypto Repository to connect with database using PrismaIO

---
+++ backend/src/modules/crypto/repositories/ICryptoRepository.ts
@@ -0,0 +1,9 @@
+import { Crypto } from "@prisma/client";
+import { ICreateCryptoDTO } from "../DTO/ICreateCryptoDTO";
+
+export interface ICryptoRepository {
+    createToken({ token, name, amount }: ICreateCryptoDTO): Promise<Crypto>
+    getAllCryptos(): Promise<Crypto[]>
+    editTokenAmount(id: string, amount: number): Promise<Crypto>
+    deleteToken(id: string): Promise<void>
+}
