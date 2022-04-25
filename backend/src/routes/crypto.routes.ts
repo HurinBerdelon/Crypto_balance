2b49f2b1155e96dd333169ea0e1ef771c68948e1
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-04-25 16:33:45 -0300

    feat: Add DeleteCrypto UseCase, Controller and Route

--- backend/src/routes/crypto.routes.ts
+++ backend/src/routes/crypto.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { CreateCryptoController } from '../modules/crypto/useCases/createCrypto/CreateCryptoController'
+import { DeleteCryptoController } from '../modules/crypto/useCases/deleteCrypto/DeleteCryptoController'
 import { GetAllCryptosController } from '../modules/crypto/useCases/getAllCryptos/GetAllCryptosController'
 
 export const cryptoRoutes = Router()
 
 const getAllCryptosController = new GetAllCryptosController()
 const createCryptoController = new CreateCryptoController()
+const deleteCryptoController = new DeleteCryptoController()
 
 cryptoRoutes.get('', getAllCryptosController.handle)
 cryptoRoutes.post('', createCryptoController.handle)
+cryptoRoutes.delete('/delete', deleteCryptoController.handle)
