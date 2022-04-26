ccc9656f4f0bb321a8a4ab935d5bc13acc271c43
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-04-26 08:51:33 -0300

    feat: Setup React project with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { GlobalStyle } from './styles/GlobalStyle';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        Hello World
+      </div>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
