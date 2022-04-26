9a176fd13cfd55b74155e7558648b8d58ffcdb2e
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-04-26 09:15:15 -0300

    feat: Add Header Component

---
+++ frontend/src/components/Header/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    height: 20%;
+    width: 100%;
+    border-radius: 1rem 1rem 0 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    background: #CCc;
+`
